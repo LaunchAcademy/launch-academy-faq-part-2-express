3029965529d99581cbb35f7373212623ff601a5a
Stephen Fang <safang99@gmail.com>
2021-01-07 17:21:36 -0500

    finished clinic code

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,21 @@
 import React from "react"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
 import { hot } from "react-hot-loader/root"
 
+import FAQList from "./FAQList"
+import LauncherList from "./LauncherList"
+import LauncherShow from "./LauncherShow"
+
 const App = (props) => {
-  return <h1>Hello from App</h1>
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={FAQList} />
+        <Route exact path="/launchers" component={LauncherList} />
+        <Route exact path="/launchers/:id" component={LauncherShow} />
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default hot(App)
